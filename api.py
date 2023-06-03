6ffd84380448311af68f4a92e568598aafd80fd1
VIJIT CHANDNA <vijit123chandna@gmail.com>
2023-06-03 23:15:27 +0530

    added youtube integration (test)

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
 """
     backend for project
 """
-
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 from flask import Flask, jsonify, request, render_template
 from bardapi import Bard
 from dotenv import load_dotenv
@@ -11,15 +12,22 @@ import os
 
 
 load_dotenv(".env")
-# bard credentials
+
+# credentials
+# yt_token = os.getenv("YOUTUBE_TOKEN")
 token = os.getenv("BARD_TOKEN")
+# youtube = build('youtube', 'v3', developerKey=yt_token)
 bard = Bard(token=token)
 # setting the scene
 scene = ""
 query_template = f"Write me some genre to search for music that would fit right with this scene: {scene} . Generate me 5 of these tags which would fit the scene and give the output in an array"
-
+# tags=[]
+# yt_query = 'copyright free music' + ''.join(tags)
 # starting flask
 app = Flask(__name__)
+
+
+
 
 
 def generate_tags(output):
@@ -51,7 +59,7 @@ def home():
 def generate():
     scene = request.args['scene']
     output = bard.get_answer(query_template)
-    if output:
+    if output!=[]:
         return (
             {'result': generate_tags(output=output)}
         )
---
+++ youtube_api.py
@@ -0,0 +1,40 @@
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# Set up the YouTube API client
+api_key = ""
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+# Define the search query
+tags = [ "ambient", "chillwave", "downtempo", "electronica", "trip hop"]
+query = 'music ' + ' '.join(tags)
+
+# Call the search.list method to retrieve search results
+try:
+    search_response = youtube.search().list(
+        q=query,
+        type='video',
+        part='id,snippet',
+        maxResults=10
+    ).execute()
+
+    # Extract the video IDs and titles from the search results
+    video_ids = []
+    video_titles = []
+
+    for search_result in search_response.get('items', []):
+        if search_result['id']['kind'] == 'youtube#video':
+            video_ids.append(search_result['id']['videoId'])
+            video_titles.append(search_result['snippet']['title'])
+
+
+    # Get the video links
+    
+
+    # Print the video IDs, titles, and links
+    print(len(video_ids),'Video IDs:', video_ids, "\n")
+    print(len(video_titles),'Video Titles:', video_titles, "\n")
+    print(len(video_links),'Video Links:', video_links, "\n")
+
+except HttpError as e:
+    print('An error occurred:', e)
