44b9c87e5f3b14dde5318e76e80148460bd82ea3
jailbreakerVC <54243183+jailbreakerVC@users.noreply.github.com>
2023-06-06 11:15:16 +0530

    frontend

--- api.py
+++ api.py
@@ -20,9 +20,12 @@ bard = Bard(token=token)
 # setting the scene
 scene = ""
 query_template = f"Write me some genre to search for music that would fit right with this scene: {scene} . Generate me 5 of these tags which would fit the scene and give the output in an array"
+# query_template = f"Write me a list of 5 genre to search for music that would fit right with this scene: {scene}, generate 5 of these tags in an array, see what type of music movies and tv shows use for this kind of scene and use that information to generate the tags."
+
 
 # starting flask
 app = Flask(__name__)
+
 
 def generate_tags(output):
     """
@@ -36,7 +39,6 @@ def generate_tags(output):
         extracted_content = extracted_content.replace(
             "\\n", "")
 
-
         extracted_content = extracted_content[2:-2].split('", "')
         return extracted_content
     else:
@@ -44,15 +46,16 @@ def generate_tags(output):
 
         )
 
+
 def get_youtube_results(tags):
- 
-    listt=[]
+
+    listt = []
     tag = tags[0].split()
-    
+
     for t in tag:
-        print(t)
-        q = str(tag) + "copyright free music" 
-        print("searching for: "+ t)
+        print("t", t)
+        q = str(t) + "copyright free music"
+        print("searching for: ", q)
         try:
             search_response = youtube.search().list(
                 q=q,
@@ -62,16 +65,20 @@ def get_youtube_results(tags):
             ).execute()
             for search_result in search_response.get('items', []):
                 if search_result['id']['kind'] == 'youtube#video':
+                    print(search_result['snippet']['title'])
                     listt.append({
-                    "video_id":search_result['id']['videoId'],
-                    "video_title":search_result['snippet']['title'],
-                    "video_link":'https://www.youtube.com/watch?v=' + search_result['id']['videoId']
-                })
+
+                        "video_id": search_result['id']['videoId'],
+                        "video_title": search_result['snippet']['title'],
+                        "video_link": 'https://www.youtube.com/watch?v=' + search_result['id']['videoId']
+                    })
         except HttpError as e:
             return {
-                    'output': "null",
-                }
+                e,
+
+            }
     return listt
+
 
 @app.route('/', methods=["GET"])
 def home():
@@ -84,11 +91,13 @@ def generate():
     scene = request.args['scene']
     output = bard.get_answer(query_template)
     if output:
+        print("getting bard output")
         tags = generate_tags(output=output)
         print(tags)
         if tags:
-            return jsonify (
-            get_youtube_results(tags=tags)
+            print("tags: ", tags)
+            return jsonify(
+                get_youtube_results(tags=tags)
             )
         else:
             return {
